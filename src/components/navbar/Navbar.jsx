b15ca95f71a16472b7cf9b27f98e6f2e47898c9a
ahm1377 <72826776+ahm1377@users.noreply.github.com>
2023-11-24 20:12:46 +0330

    add site name with variable

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 import styled from "./Navbar.module.css"
 
 function Navbar(){
-    return(
+  let title="علی بلاگ"  
+  return(
       <div className={styled.header}>
+        <h3>{title}</h3>
         <ul>
           <li>لیست مقالات</li>
           <li>ساخت مقاله</li>
